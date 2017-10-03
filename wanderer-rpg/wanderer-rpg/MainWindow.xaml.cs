7d47deb25d1bd3674f4025383850ecdeebc45784
evi <evelinhlacsok@gmail.com>
2017-10-03 13:38:15 +0200

    Tile-class-created-works

--- wanderer-rpg/wanderer-rpg/MainWindow.xaml.cs
+++ wanderer-rpg/wanderer-rpg/MainWindow.xaml.cs
@@ -14,24 +14,9 @@ namespace RPGGame
         {
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
-            DrawTile();
+            Tile.DrawTile(foxDraw);
         }
-        public void DrawTile()
-        {
-            var foxDraw = new FoxDraw(canvas);
-            int x = 0;
-            int y = 0;
-            for (int i = 1; i <= 10; i++)
-            {
-                y = i * 50;
-                for (int j = 1; j <= 10; j++)
-                {
-                    x = j * 50;
-                    foxDraw.AddImage("./Assets/floor.png", x, y);
-                    
-                }
-            }
-        }
+        
         private void WindowKeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Left)
---
+++ wanderer-rpg/wanderer-rpg/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreenFox;
+
+
+namespace RPGGame
+{
+    class Tile
+    {
+        public static void DrawTile(FoxDraw foxDraw)
+        {
+            int x = 0;
+            int y = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                y = i * 50;
+                for (int j = 1; j <= 10; j++)
+                {
+                    x = j * 50;
+                    foxDraw.AddImage("./Assets/floor.png", x, y);
+
+                }
+            }
+            foxDraw.AddImage("./Assets/hero-down.png", 50, 50);
+        }
+    }
+}
