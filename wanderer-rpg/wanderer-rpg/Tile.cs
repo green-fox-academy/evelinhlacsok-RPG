b94687d592258bbe245303e1ae57a7254604a2aa
evi <evelinhlacsok@gmail.com>
2017-10-03 16:06:16 +0200

    having the map with floors and walls

--- wanderer-rpg/wanderer-rpg/Tile.cs
+++ wanderer-rpg/wanderer-rpg/Tile.cs
@@ -15,53 +15,41 @@ namespace RPGGame
         {
             int x = 0;
             int y = 0;
-            for (int i = 1; i <= 10; i++)
-            {
-                y = i * 50;
-                for (int j = 1; j <= 10; j++)
-                {
-                    x = j * 50;
-                    foxDraw.AddImage("./Assets/floor.png", x, y);
-                }
-            }
-            foxDraw.AddImage("./Assets/hero-down.png", 50, 50);
-            // string path = ("./Assets/map.txt");
-            // ReadFile(path);
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    y = i * 50;
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        x = j * 50;
+            //        foxDraw.AddImage("./Assets/floor.png", x, y);
+            //    }
+            // }
 
             StreamReader myReader = new StreamReader(@"\\Mac\Home\Documents\greenfox\evelinhlacsok-RPG\wanderer-rpg\wanderer-rpg\Assets\map.txt");
             string line = "";
             while (line != null)
             {
-                line = myReader.ReadLines();
+                line = myReader.ReadLine();
+                y = y + 50;
+                x = 0;
                 if (line != null)
                 {
                     string[] letters = line.Split(' ');
                     foreach (var letter in letters)
                     {
-                        if (letter == "f")
-                        {
-                            foxDraw.AddImage("./Assets/floor.png", x, y);
-                        }
-                        else
+                        x = x + 50;
+                        if (letter == "w")
                         {
                             foxDraw.AddImage("./Assets/wall.png", x, y);
                         }
-
+                        else
+                        {
+                            foxDraw.AddImage("./Assets/floor.png", x, y);
+                        }
                     }
                 }
             }
-
-
-            //foxDraw.AddImage("./Assets/wall.png", 200, 50);
-            //foxDraw.AddImage("./Assets/wall.png", 200, 100);
-            //foxDraw.AddImage("./Assets/wall.png", 200, 150);
-            //foxDraw.AddImage("./Assets/wall.png", 150, 150);
-            //foxDraw.AddImage("./Assets/wall.png", 100, 150);
-            
+            foxDraw.AddImage("./Assets/hero-down.png", 50, 50);
         }
-        //static void ReadFile(string path)
-        //{
-        //    File.ReadAllText(path);
-        //}
     }
 }
