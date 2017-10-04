82cbb5edb92bc1f00c9093d16ee370cb00fc4c57
evi <evelinhlacsok@gmail.com>
2017-10-04 15:34:23 +0200

    matrix creation

--- wanderer-rpg/wanderer-rpg/Map.cs
+++ wanderer-rpg/wanderer-rpg/Map.cs
@@ -11,6 +11,8 @@ namespace RPGGame
 {
     class Map
     {
+        static int[,] matrix = new int[10, 10];
+       
         public static void DrawMap(FoxDraw foxDraw)
         {
             int x = 0;
@@ -18,26 +20,30 @@ namespace RPGGame
             
             StreamReader myReader = new StreamReader(@"\\Mac\Home\Documents\greenfox\evelinhlacsok-RPG\wanderer-rpg\wanderer-rpg\Assets\map.txt");
             string line = "";
+
             while (line != null)
             {
                 line = myReader.ReadLine();
-                y = y + 50;
+                y = y + 1;
                 x = 0;
                 if (line != null)
                 {
                     string[] letters = line.Split(' ');
                     foreach (var letter in letters)
                     {
-                        x = x + 50;
+                        x = x + 1;
                         if (letter == "w")
                         {
-                            foxDraw.AddImage("./Assets/wall.png", x, y);
+                            matrix[x, y] = 1;
+                            foxDraw.AddImage("./Assets/wall.png", x * 50, y * 50);
                         }
                         else
                         {
-                            foxDraw.AddImage("./Assets/floor.png", x, y);
+                            matrix[x, y] = 0;
+                            foxDraw.AddImage("./Assets/floor.png", x * 50, y * 50);
                         }
                     }
+                      
                 }
             }
         }
